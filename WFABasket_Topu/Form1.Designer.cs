ed841fe4d6159969e0aa34c6776bdff6ed323fbe
AdamGiby <45285832+AdamGiby@users.noreply.github.com>
2019-01-19 20:20:38 +0300

    Add files via upload

---
+++ WFABasket_Topu/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WFABasket_Topu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tmrTop = new System.Windows.Forms.Timer(this.components);
+            this.pbBasketTopu = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBasketTopu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tmrTop
+            // 
+            this.tmrTop.Enabled = true;
+            this.tmrTop.Interval = 1000;
+            this.tmrTop.Tick += new System.EventHandler(this.tmrTop_Tick);
+            // 
+            // pbBasketTopu
+            // 
+            this.pbBasketTopu.Image = ((System.Drawing.Image)(resources.GetObject("pbBasketTopu.Image")));
+            this.pbBasketTopu.Location = new System.Drawing.Point(99, 75);
+            this.pbBasketTopu.Name = "pbBasketTopu";
+            this.pbBasketTopu.Size = new System.Drawing.Size(182, 150);
+            this.pbBasketTopu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbBasketTopu.TabIndex = 0;
+            this.pbBasketTopu.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 844);
+            this.Controls.Add(this.pbBasketTopu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbBasketTopu)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer tmrTop;
+        private System.Windows.Forms.PictureBox pbBasketTopu;
+    }
+}
+
---
+++ WFABasket_Topu/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFABasket_Topu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Properties.Resources.saha;
+            tmrTop.Start();
+        }
+
+        private void tmrTop_Tick(object sender, EventArgs e)
+        {
+            
+            pbBasketTopu.Left = rnd.Next(0, (this.Width - pbBasketTopu.Width ));
+            pbBasketTopu.Top = rnd.Next(0, (this.Height - pbBasketTopu.Height ));
+            if (pbBasketTopu.Bottom >=this.Height) //Width
+            {
+                pbBasketTopu.Left -= 30; //+
+                pbBasketTopu.Top  -= 20;
+            }
+           else if (pbBasketTopu.Right >= this.Width) //Heighr
+            {
+                pbBasketTopu.Left += 30;
+                pbBasketTopu.Top  += 20;
+            }
+           //else if(pbBasketTopu.Top >= this.Width) 
+            //{
+              //  pbBasketTopu.Left -= 30;
+                //pbBasketTopu.Top += 20;
+            //}
+            //else if(pbBasketTopu.Left >= this.Height) 
+            //{
+              //  pbBasketTopu.Left -= 30;
+               // pbBasketTopu.Top -= 20;
+            //}
+        }
+
+        
+
+
+    }
+
+}
